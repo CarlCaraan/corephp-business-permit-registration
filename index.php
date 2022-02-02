8a66281101baa608ca62898cb78ff39e3b899e56
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-02-02 11:05:33 +0800

    2/2/22

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<?php include 'includes/head.php'; ?>
 	<title>Business Permit Registration</title>
 	<link rel="stylesheet" href="assets/css/animate.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body id="landingpage_body">
@@ -143,6 +144,127 @@
 			</div>
 		</section>
 		<!--========= End Steps Instructions Section =========-->
+
+		<!--========= Start General FAQs Section =========-->
+		<section class="faq py-5">
+			<!-- Heading Text -->
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center wow zoomIn" data-wow-duration=".4s" data-wow-delay=".4s">
+						<h4 id="landing_page_heading">GENERAL FAQs</h4>
+						<div class="sub-heading">
+							<p>
+								You can ask the question regarding the the registration process
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="container">
+				<div class="row center">
+					<div class="col-md-8">
+						<div class="panel-group" id="accordian">
+							<div class="panel panel-default wow fadeInUp" data-wow-duration=".3s" data-wow-delay=".3s">
+								<div class="panel-heading" id="headingOne">
+									<h4 class="panel-title">
+										<a href="#collapseOne" data-toggle="collapse" data-parent="#accordian">Have you any questions ?</a>
+									</h4>
+								</div>
+								<div id="collapseOne" class="panel-collapse collapse in" aria-labelledby="headingone">
+									<div class="panel-body">
+										<p class="step-body">
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nisl lorem, dictum id pellentesque at, vestibulum ut arcu. Curabitur erat libero, egestas eu tincidunt ac, rutrum ac justo. Vivamus condimentum laoreet lectus, blandit posuere tortor aliquam vitae. Curabitur molestie eros.
+										</p>
+									</div>
+								</div>
+							</div>
+							<div class="panel panel-default wow fadeInUp" data-wow-duration=".6s" data-wow-delay=".6s">
+								<div class="panel-heading" id="headingTwo">
+									<h4 class="panel-title">
+										<a href="#collapseTwo" class="collapsed" data-toggle="collapse" data-parent="#accordian">Are you getting it exactly ?</a>
+									</h4>
+								</div>
+								<div id="collapseTwo" class="panel-collapse collapse" aria-labelledby="headingTwo">
+									<div class="panel-body">
+										<p class="step-body">
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nisl lorem, dictum id pellentesque at, vestibulum ut arcu. Curabitur erat libero, egestas eu tincidunt ac, rutrum ac justo. Vivamus condimentum laoreet lectus, blandit posuere tortor aliquam vitae. Curabitur molestie eros.
+										</p>
+									</div>
+								</div>
+							</div>
+							<div class="panel panel-default wow fadeInUp" data-wow-duration=".9s" data-wow-delay=".9s">
+								<div class="panel-heading" id="headingThree">
+									<h4 class="panel-title">
+										<a href="#collapseThree" class="collapsed" data-toggle="collapse" data-parent="#accordian">Is it clearly explained ?</a>
+									</h4>
+								</div>
+								<div id="collapseThree" class="panel-collapse collapse" aria-labelledby="headingThree">
+									<div class="panel-body">
+										<p class="step-body">
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nisl lorem, dictum id pellentesque at, vestibulum ut arcu. Curabitur erat libero, egestas eu tincidunt ac, rutrum ac justo. Vivamus condimentum laoreet lectus, blandit posuere tortor aliquam vitae. Curabitur molestie eros.
+										</p>
+									</div>
+								</div>
+							</div>
+							<div class="panel panel-default wow fadeInUp" data-wow-duration="1.2s" data-wow-delay="1.2s">
+								<div class="panel-heading" id="headingfour">
+									<h4 class="panel-title">
+										<a href="#collapsefour" class="collapsed" data-toggle="collapse" data-parent="#accordian"> Course is deserving anything ?</a>
+									</h4>
+								</div>
+								<div id="collapsefour" class="panel-collapse collapse" aria-labelledby="headingfour">
+									<div class="panel-body">
+										<p class="step-body">
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nisl lorem, dictum id pellentesque at, vestibulum ut arcu. Curabitur erat libero, egestas eu tincidunt ac, rutrum ac justo. Vivamus condimentum laoreet lectus, landit posuere tortor aliquam vitae. Curabitur molestie eros.
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--========= End General FAQs Section =========-->
+
+		<!--========= Start Map Section =========-->
+		<section id="map_section">
+			<div class="container-fluid" id="map_heading_container">
+				<div class="row">
+					<div class="col-md-4 text-center">
+						<div class="contact-item wow fadeInLeft" data-wow-duration=".3s" data-wow-delay=".3s">
+							<i class="fas fa-map-marker-alt fa-2x text-white my-2"></i>
+							<p class="contact-detail text-white">
+								Real Velasquez Street,
+								Poblacion Dos<br>
+								Santa Maria, Laguna
+							</p>
+						</div>
+					</div>
+					<div class="col-md-4 text-center">
+						<div class="contact-item wow fadeInLeft" data-wow-duration=".6s" data-wow-delay=".6s">
+							<i class="fas fa-envelope fa-2x text-white my-2"></i>
+							<p class="contact-detail text-white">
+								santamarialaguna@gmail.com <br>
+								supportsantamaria@gmail.com
+							</p>
+						</div>
+					</div>
+					<div class="col-md-4 text-center">
+						<div class="contact-item wow fadeInLeft" data-wow-duration=".9s" data-wow-delay=".9s">
+							<i class="fas fa-phone fa-2x text-white my-2"></i>
+							<p class="contact-detail text-white">
+								+123-456-7890 - Municipal Office<br>
+								+123-456-7890 - Fax
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<iframe class="wow fadeInUp" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1365.864254205747!2d121.42317492028425!3d14.470472255620722!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397ede77c57074d%3A0xcde0b295efdad3a3!2sMunicipal%20Hall%20of%20Santa%20Maria%2C%20Laguna!5e0!3m2!1sen!2sph!4v1643769101653!5m2!1sen!2sph" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</section>
+		<!--========= End Map Section =========-->
 
 		<!--========= Start Footer Section =========-->
 		<?php include 'includes/user_footer.php'; ?>
