742c1e042cf31235508af055b7c9b763bb3193a8
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-02-10 13:15:40 +0800

    2/10/22

--- gconfig.php
+++ gconfig.php
@@ -4,11 +4,11 @@ require_once 'vendor/autoload.php';
 
 $google_client = new Google_Client();
 
-$google_client->setClientId('1027852877992-gmjnevvt61vjmo17nsbi2hi6jhb21fpo.apps.googleusercontent.com');
+$google_client->setClientId('1027852877992-gmjnevvt61vjmo17nsbi2hi6jhb21fpo.apps.googleusercontent.com'); // Set Client Id
 
 $google_client->setClientSecret('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');
 
-$google_client->setRedirectUri('http://localhost/Business%20Permit%20Registration/adminhome.php');
+$google_client->setRedirectUri('http://localhost/Business%20Permit%20Registration/adminhome.php'); // Set Secret Id
 
 $google_client->addScope('email');
 
