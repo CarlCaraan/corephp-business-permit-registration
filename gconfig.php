46eed8e665cf7032989a80152015cd1f41aaae23
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-02-10 13:19:03 +0800

    2/10/22

--- gconfig.php
+++ gconfig.php
@@ -6,10 +6,9 @@ $google_client = new Google_Client();
 
 $google_client->setClientId('1027852877992-gmjnevvt61vjmo17nsbi2hi6jhb21fpo.apps.googleusercontent.com'); // Set Client Id
 
-$google_client->setClientSecret('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');
+$google_client->setClientSecret('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'); // Set Secret Id
 
-$google_client->setRedirectUri('http://localhost/Business%20Permit%20Registration/adminhome.php'); // Set Secret Id
-
+$google_client->setRedirectUri('http://localhost/Business%20Permit%20Registration/adminhome.php'); // Set Url to redirect after sign in
 $google_client->addScope('email');
 
 $google_client->addScope('profile');
